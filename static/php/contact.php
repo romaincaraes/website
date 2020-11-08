57eafa901b4c2c51b08e48d03b8a6f6371268dd8
romaincaraes <hello@romaincaraes.fr>
2020-11-08 18:41:09 +0100

    Update static/php/contact.php

---
+++ static/php/contact.php
@@ -0,0 +1,66 @@
+<?php 
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <title>Romain Caraës</title>
+
+        <link rel="author" type="text/plain" href="/humans.txt"/>
+        <link rel="icon" href="/static/img/favicon.ico"/>
+        <link rel="apple-touch-icon" href="/static/img/icon.png"/>
+
+        <meta name="description" content="">
+        <meta name="author" content="Romain Caraës">
+
+        <link type="text/css" rel="stylesheet" href="//fonts.googleapis.com/css?family=Inter:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i">
+        <link type="text/css" rel="stylesheet" href="//cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+        <link type="text/css" rel="stylesheet" href="//use.fontawesome.com/releases/v5.15.1/css/all.css">
+        <link type="text/css" rel="stylesheet" href="/static/css/style.css">
+    </head>
+    <body class="bg-dark">
+        <div class="container h-100">
+            <div class="row justify-content-center h-100">
+                <div class="col-xl-5 col-lg-6 col-md-10 col-sm-12 m-auto">
+                    <div class="card">
+                        <div class="card-body">
+                            <h1 class="card-title">Contact me</h1>
+                            <form class="form-contact" action="/static/vcf/romaincaraes.vcf" method="get">
+                                <ul class="card-text list-group p-2">
+                                    <li class="list-group-item d-flex justify-content-between">
+                                        Phone. <a href="tel:+33123456789">+33 (0)1 23 45 67 89</a>
+                                    </li>
+                                    <li class="list-group-item d-flex justify-content-between">
+                                        Email. <a href="mailto:email@domain.com">email@domain.com</a>
+                                    </li>
+                                    <li class="list-group-item d-flex justify-content-between">
+                                        Website. <a href="//romaincaraes.fr" target="_blank">https://romaincaraes.fr</a>
+                                    </li>
+                                    <li class="list-group-item d-flex justify-content-between">
+                                        LinkedIn. <a href="//linkedin.com/in/romaincaraes" target="_blank">Romain Caraës</a>
+                                    </li>
+                                    <li class="list-group-item d-flex justify-content-between">
+                                        Twitter. <a href="//twitter.com/romaincaraes" target="_blank">@romaincaraes</a>
+                                    </li>
+                                    <li class="list-group-item d-flex justify-content-between">
+                                        GitHub. <a href="//github.com/romaincaraes" target="_blank">@romaincaraes</a>
+                                    </li>
+                                </ul>
+                                <div class="text-center">
+                                    <button type="submit" class="btn btn-primary"><i class="fas fa-address-card mr-2"></i>Download</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <script src="//code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="//cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+        <script src="/static/js/script.js"></script>
+    </body>
+</html>
