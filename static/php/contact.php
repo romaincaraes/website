a513cb2ded94bfca493586be160b1467b46fa389
romaincaraes <hello@romaincaraes.fr>
2020-11-16 16:24:17 +0100

    Simplify QR code contact form
    Move templates/contact.html code into static/php/contact.php

--- static/php/contact.php
+++ static/php/contact.php
@@ -16,6 +16,124 @@
     return $result;
     }
 
+    $input = <<<EOL
+    <form class="form-contact text-primary" action="/static/php/contact.php" method="post">
+        <div class="form-label-group mb-3">
+            <div class="input-group">
+                <div class="input-group-prepend">
+                    <span class="fa-stack fa-1x">
+                        <i class="fas fa-circle fa-stack-2x"></i>
+                        <i class="fas fa-user fa-stack-1x fa-inverse"></i>
+                    </span>
+                </div>
+                <input type="text" class="form-control rounded-pill mx-1" id="inputFirstName" name="inputFirstName" aria-label="First Name" placeholder="First Name" required>
+                <input type="text" class="form-control rounded-pill mx-1" id="inputLastName" name="inputLastName" aria-label="Last Name" placeholder="Last Name" required>
+            </div>
+        </div>
+        <div class="form-label-group mb-3">
+            <div class="input-group">
+                <div class="input-group-prepend">
+                    <span class="fa-stack fa-1x">
+                        <i class="fas fa-circle fa-stack-2x"></i>
+                        <i class="fas fa-mobile fa-stack-1x fa-inverse"></i>
+                    </span>
+                </div>
+                <input type="tel" class="form-control rounded-pill mx-1" id="inputPhone" name="inputPhone" aria-label="Phone" placeholder="Phone" required>
+            </div>
+        </div>
+        <div class="form-label-group mb-3">
+            <div class="input-group">
+                <div class="input-group-prepend">
+                    <span class="fa-stack fa-1x">
+                        <i class="fas fa-circle fa-stack-2x"></i>
+                        <i class="fas fa-envelope fa-stack-1x fa-inverse"></i>
+                    </span>
+                </div>
+                <input type="email" class="form-control rounded-pill mx-1" id="inputEmail" name="inputEmail" aria-label="Email" placeholder="Email" required>
+            </div>
+        </div>
+        <div class="text-center">
+            <button type="submit" class="btn btn-primary rounded-pill"><i class="fas fa-paper-plane mr-2"></i>Submit</button>
+        </div>
+    </form>
+    EOL;
+
+    $output = <<<EOL
+    <form class="form-contact text-primary" action="/static/vcf/romaincaraes.vcf" method="get">
+        <div class="form-label-group mb-3">
+            <div class="input-group">
+                <div class="input-group-prepend">
+                    <span class="fa-stack fa-1x">
+                        <i class="fas fa-circle fa-stack-2x"></i>
+                        <i class="fas fa-mobile fa-stack-1x fa-inverse"></i>
+                    </span>
+                </div>
+                <a class="form-control btn btn-light rounded-pill mx-1" href="tel:+33123456789">+33 (0)1 23 45 67 89</a>
+            </div>
+        </div>
+        <div class="form-label-group mb-3">
+            <div class="input-group">
+                <div class="input-group-prepend">
+                    <span class="fa-stack fa-1x">
+                        <i class="fas fa-circle fa-stack-2x"></i>
+                        <i class="fas fa-envelope fa-stack-1x fa-inverse"></i>
+                    </span>
+                </div>
+                <a class="form-control btn btn-light rounded-pill mx-1" href="mailto:email@domain.com">email@domain.com</a>
+            </div>
+        </div>
+        <div class="form-label-group mb-3">
+            <div class="input-group">
+                <div class="input-group-prepend">
+                    <span class="fa-stack fa-1x">
+                        <i class="fas fa-circle fa-stack-2x"></i>
+                        <i class="fas fa-desktop fa-stack-1x fa-inverse"></i>
+                    </span>
+                </div>
+                <a class="form-control btn btn-light rounded-pill mx-1" href="//romaincaraes.fr" target="_blank">romaincaraes.fr</a>
+            </div>
+        </div>
+        <div class="form-label-group mb-3">
+            <div class="input-group">
+                <div class="input-group-prepend">
+                    <span class="fa-stack fa-1x">
+                        <i class="fas fa-circle fa-stack-2x"></i>
+                        <i class="fab fa-linkedin fa-stack-1x fa-inverse"></i>
+                    </span>
+                </div>
+                <a class="form-control btn btn-light rounded-pill mx-1" href="//linkedin.com/in/romaincaraes" target="_blank">Romain Caraës</a>
+            </div>
+        </div>
+        <div class="form-label-group mb-3">
+            <div class="input-group">
+                <div class="input-group-prepend">
+                    <span class="fa-stack fa-1x">
+                        <i class="fas fa-circle fa-stack-2x"></i>
+                        <i class="fab fa-twitter fa-stack-1x fa-inverse"></i>
+                    </span>
+                </div>
+                <a class="form-control btn btn-light rounded-pill mx-1" href="//twitter.com/romaincaraes" target="_blank">@romaincaraes</a>
+            </div>
+        </div>
+        <div class="form-label-group mb-3">
+            <div class="input-group">
+                <div class="input-group-prepend">
+                    <span class="fa-stack fa-1x">
+                        <i class="fas fa-circle fa-stack-2x"></i>
+                        <i class="fab fa-github fa-stack-1x fa-inverse"></i>
+                    </span>
+                </div>
+                <a class="form-control  btn btn-light rounded-pill mx-1" href="//github.com/romaincaraes" target="_blank">@romaincaraes</a>
+            </div>
+        </div>
+        <div class="text-center">
+            <button type="submit" class="btn btn-primary rounded-pill"><i class="fas fa-address-card mr-2"></i>Download</button>
+        </div>
+    </form>
+    EOL;
+
+    $card = $input;
+
     if (!empty($_POST)) {
         extract($_POST);
 
@@ -44,7 +162,6 @@
             $message .= "<p>Name : " . $inputName . "</p>";
             $message .= "<p>Email : " . $inputEmail . "</p>";
             $message .= "<p>Phone : " . $inputPhone . "</p>";
-
             $header = "From: " . $inputName . " <" . $inputEmail . ">" . $br;
             $header .= "Reply-To: " . $to . $br;
             $header .= "Content-Type: text/html; charset=utf-8" . $br;
@@ -52,6 +169,7 @@
             $sent = mail($to, $subject, $message, utf8_decode($header));
             if ($sent) {
                 $error = "Your message was sent !";
+                $card = $output;
             }
             else {
                 $error = "An error occured, please start again !";
@@ -89,77 +207,7 @@
                     <div class="card bg-dark text-light border-0">
                         <div class="card-body">
                             <h1 class="card-title mb-3">Contact</h1>
-                            <form class="form-contact text-primary" action="/static/vcf/romaincaraes.vcf" method="get">
-                                <div class="form-label-group mb-3">
-                                    <div class="input-group">
-                                        <div class="input-group-prepend">
-                                            <span class="fa-stack fa-1x">
-                                                <i class="fas fa-circle fa-stack-2x"></i>
-                                                <i class="fas fa-mobile fa-stack-1x fa-inverse"></i>
-                                            </span>
-                                        </div>
-                                        <a class="form-control btn btn-light rounded-pill mx-1" href="tel:+33123456789">+33 (0)1 23 45 67 89</a>
-                                    </div>
-                                </div>
-                                <div class="form-label-group mb-3">
-                                    <div class="input-group">
-                                        <div class="input-group-prepend">
-                                            <span class="fa-stack fa-1x">
-                                                <i class="fas fa-circle fa-stack-2x"></i>
-                                                <i class="fas fa-envelope fa-stack-1x fa-inverse"></i>
-                                            </span>
-                                        </div>
-                                        <a class="form-control btn btn-light rounded-pill mx-1" href="mailto:email@domain.com">email@domain.com</a>
-                                    </div>
-                                </div>
-                                <div class="form-label-group mb-3">
-                                    <div class="input-group">
-                                        <div class="input-group-prepend">
-                                            <span class="fa-stack fa-1x">
-                                                <i class="fas fa-circle fa-stack-2x"></i>
-                                                <i class="fas fa-desktop fa-stack-1x fa-inverse"></i>
-                                            </span>
-                                        </div>
-                                        <a class="form-control btn btn-light rounded-pill mx-1" href="//romaincaraes.fr" target="_blank">romaincaraes.fr</a>
-                                    </div>
-                                </div>
-                                <div class="form-label-group mb-3">
-                                    <div class="input-group">
-                                        <div class="input-group-prepend">
-                                            <span class="fa-stack fa-1x">
-                                                <i class="fas fa-circle fa-stack-2x"></i>
-                                                <i class="fab fa-linkedin fa-stack-1x fa-inverse"></i>
-                                            </span>
-                                        </div>
-                                        <a class="form-control btn btn-light rounded-pill mx-1" href="//linkedin.com/in/romaincaraes" target="_blank">Romain Caraës</a>
-                                    </div>
-                                </div>
-                                <div class="form-label-group mb-3">
-                                    <div class="input-group">
-                                        <div class="input-group-prepend">
-                                            <span class="fa-stack fa-1x">
-                                                <i class="fas fa-circle fa-stack-2x"></i>
-                                                <i class="fab fa-twitter fa-stack-1x fa-inverse"></i>
-                                            </span>
-                                        </div>
-                                        <a class="form-control btn btn-light rounded-pill mx-1" href="//twitter.com/romaincaraes" target="_blank">@romaincaraes</a>
-                                    </div>
-                                </div>
-                                <div class="form-label-group mb-3">
-                                    <div class="input-group">
-                                        <div class="input-group-prepend">
-                                            <span class="fa-stack fa-1x">
-                                                <i class="fas fa-circle fa-stack-2x"></i>
-                                                <i class="fab fa-github fa-stack-1x fa-inverse"></i>
-                                            </span>
-                                        </div>
-                                        <a class="form-control  btn btn-light rounded-pill mx-1" href="//github.com/romaincaraes" target="_blank">@romaincaraes</a>
-                                    </div>
-                                </div>
-                                <div class="text-center">
-                                    <button type="submit" class="btn btn-primary rounded-pill"><i class="fas fa-address-card mr-2"></i>Download</button>
-                                </div>
-                            </form>
+                            <?php echo $card; ?>
                         </div>
                     </div>
                 </div>
