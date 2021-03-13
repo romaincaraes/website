40889dc1bfb1ee25e3355f339ecaf497dffd655f
romaincaraes <hello@romaincaraes.fr>
2021-03-13 19:49:06 +0100

    Add a footer line to contact.php with a link to website's GitHub repository

--- static/php/contact.php
+++ static/php/contact.php
@@ -215,6 +215,11 @@
                 </div>
             </div>
         </div>
+        <footer class="footer">
+            <div class="container mt-n5 mb-5">
+                <span class="text-muted">This website's source code is available on <a href="https://github.com/romaincaraes/website">github.com/romaincaraes/website</a></span>
+            </div>
+        </footer>
         <script src="//code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
         <script src="//cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
         <script src="/static/js/script.js"></script>
