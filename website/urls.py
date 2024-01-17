df4161bd31a17cec5de44288aab3b276301ab352
Romain Caraës <hello@romaincaraes.fr>
2024-01-17 23:30:41 +0100

    Update website/urls.py

--- website/urls.py
+++ website/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import include, path
 from .views import index
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', index, name='index'),
     path('contact/', include('contact.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
