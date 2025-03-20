6dcc15a6b52a8a94491fa711ea326409dc31e69e
Romain Caraës <hello@romaincaraes.fr>
2025-03-20 23:56:42 +0100

    Update contact/templates/contact.html

--- contact/views.py
+++ contact/views.py
@@ -1,5 +1,8 @@
 import os
+from dotenv import load_dotenv
 from django.shortcuts import render
+
+load_dotenv()
 
 def contact(request):
     context = {
@@ -10,5 +13,7 @@ def contact(request):
         'linkedin_url': os.environ.get('LINKEDIN_URL'),
         'github_label': os.environ.get('GITHUB_LABEL'),
         'github_url': os.environ.get('GITHUB_URL'),
+        'form_display': "",
+        'card_display': "hidden",
     }
     return render(request, 'contact.html', context=context)
